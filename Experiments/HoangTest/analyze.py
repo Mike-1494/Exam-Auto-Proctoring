c3644806d3d8a15c0460072ba827e4df482df57e
hoanglmv <131538387+hoanglmv@users.noreply.github.com>
2023-08-11 16:07:26 +0700

    convert video to image

--- Experiments/HoangTest/analyze.py
+++ Experiments/HoangTest/analyze.py
@@ -1,16 +1,15 @@
 from ultralytics import YOLO 
 import cv2
 model = YOLO('yolov8n.pt')
-cap = cv2.VideoCapture('"D:\Git\Exam-Auto-Proctoring\Experiments\HoangTest\dance.mp4"')
-fps = cap.get(cv2.CAP_PROP_FPS)
+cap = cv2.VideoCapture('D:\Git\Exam-Auto-Proctoring\Experiments\HoangTest\dance.mp4')
 count = 0
 
 while True:
     ret, frame = cap.read()
     results = model(frame)
     cv2.imshow('Frame', frame)
-    if count % fps == 0:
-        cv2.imwrite(f'image_{count}.jpg', frame)
+    cv2.imwrite(f'D:\Git\Exam-Auto-Proctoring\Experiments\HoangTest\Image\image_{count}.jpg', frame) 
+    count += 1 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
